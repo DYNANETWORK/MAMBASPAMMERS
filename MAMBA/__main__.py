4fe9a7e81aaacf5fa03c790ea84e6ba6c3107420
SUKHPAL <86770159+SUKHPAL443@users.noreply.github.com>
2022-02-01 18:47:34 +0530

    Update __main__.py

--- MAMBA/__main__.py
+++ MAMBA/__main__.py
@@ -8,13 +8,13 @@ from pathlib import Path
 from MAMBA.utils import load_plugins
 import logging
 from telethon import events
-from . import BOT0, BOT1, BOT2, BOT3, BOT4, BOT5, BOT6, BOT7, BOT8, BOT9,BOT0, BOT1, BOT2, BOT3, BOT4, BOT5, BOT6, BOT7, BOT8, BOT9BOT0, BOT1, BOT2, BOT3, BOT4, BOT5, BOT6, BOT7, BOT8, BOT9BOT0, BOT1, BOT2, BOT3, BOT4, BOT5, BOT6, BOT7, BOT8, BOT9BOT0, BOT1, BOT2, BOT3, BOT4, BOT5, BOT6, BOT7, BOT8, BOT9BOT0, BOT1, BOT2, BOT3, BOT4, BOT5, BOT6, BOT7, BOT8, BOT9BOT0, BOT1, BOT2, BOT3, BOT4, BOT5, BOT6, BOT7, BOT8, BOT9BOT0, BOT1, BOT2, BOT3, BOT4, BOT5, BOT6, BOT7, BOT8, BOT9BOT0, BOT1, BOT2, BOT3, BOT4, BOT5, BOT6, BOT7, BOT8, BOT9
+from . import BOT0, BOT1, BOT2, BOT3, BOT4, BOT5, BOT6, BOT7, BOT8, BOT9, BOT10, BOT11, BOT12, BOT13, BOT14, BOT15, BOT16, BOT17, BOT18, BOT19,BOT20, BOT21, BOT22, BOT23, BOT24, BOT25, BOT26, BOT27, BOT28, BOT29, BOT30, BOT31, BOT32, BOT33, BOT34, BOT35, BOT36, BOT37, BOT38, BOT39, BOT40, BOT41, BOT42, BOT43, BOT44, BOT45, BOT46, BOT47, BOT48, BOT49, BOT50, BOT51, BOT52, BOT53, BOT54, BOT55, BOT56, BOT57, BOT58, BOT59, BOT60
 
 logging.basicConfig(format='[%(levelname) 5s/%(asctime)s] %(name)s: %(message)s',
                     level=logging.WARNING)
 
 
-path = "Godfather/plugins/*.py"
+path = "MAMBA/plugins/*.py"
 files = glob.glob(path)
 for name in files:
     with open(name) as a:
@@ -54,3 +54,155 @@ if __name__ == "__main__":
 
 if __name__ == "__main__":
     BOT9.run_until_disconnected()    
+    if __name__ == "__main__":
+    BOT10.run_until_disconnected()
+    
+if __name__ == "__main__":
+    BOT11.run_until_disconnected()
+
+if __name__ == "__main__":
+    BOT12.run_until_disconnected()
+    
+if __name__ == "__main__":
+    BOT13.run_until_disconnected()
+
+if __name__ == "__main__":
+    BOT14.run_until_disconnected()
+    
+if __name__ == "__main__":
+    BOT15.run_until_disconnected()
+    
+if __name__ == "__main__":
+    BOT16.run_until_disconnected()
+
+if __name__ == "__main__":
+    BOT17.run_until_disconnected()
+    
+if __name__ == "__main__":
+    BOT18.run_until_disconnected()
+    
+if __name__ == "__main__":
+    BOT19.run_until_disconnected() 
+    
+if __name__ == "__main__":
+    BOT20.run_until_disconnected()
+    
+if __name__ == "__main__":
+    BOT21.run_until_disconnected()
+
+if __name__ == "__main__":
+    BOT22.run_until_disconnected()
+    
+if __name__ == "__main__":
+    BOT23.run_until_disconnected()
+
+if __name__ == "__main__":
+    BOT24.run_until_disconnected()
+    
+if __name__ == "__main__":
+    BOT25.run_until_disconnected()
+    
+if __name__ == "__main__":
+    BOT26.run_until_disconnected()
+
+if __name__ == "__main__":
+    BOT27.run_until_disconnected()
+    
+if __name__ == "__main__":
+    BOT28.run_until_disconnected()
+
+if __name__ == "__main__":
+    BOT29.run_until_disconnected()    
+if __name__ == "__main__":
+    BOT30.run_until_disconnected()
+    
+if __name__ == "__main__":
+    BOT31.run_until_disconnected()
+
+if __name__ == "__main__":
+    BOT32.run_until_disconnected()
+    
+if __name__ == "__main__":
+    BOT33.run_until_disconnected()
+
+if __name__ == "__main__":
+    BOT34.run_until_disconnected()
+    
+if __name__ == "__main__":
+    BOT35.run_until_disconnected()
+    
+if __name__ == "__main__":
+    BOT36.run_until_disconnected()
+
+if __name__ == "__main__":
+    BOT37.run_until_disconnected()
+    
+if __name__ == "__main__":
+    BOT38.run_until_disconnected()
+
+if __name__ == "__main__":
+    BOT39.run_until_disconnected()
+    
+if __name__ == "__main__":
+    BOT40.run_until_disconnected()
+    
+if __name__ == "__main__":
+    BOT41.run_until_disconnected()
+
+if __name__ == "__main__":
+    BOT42.run_until_disconnected()
+    
+if __name__ == "__main__":
+    BOT43.run_until_disconnected()
+
+if __name__ == "__main__":
+    BOT44.run_until_disconnected()
+    
+if __name__ == "__main__":
+    BOT45.run_until_disconnected()
+    
+if __name__ == "__main__":
+    BOT46.run_until_disconnected()
+
+if __name__ == "__main__":
+    BOT47.run_until_disconnected()
+    
+if __name__ == "__main__":
+    BOT48.run_until_disconnected()
+
+if __name__ == "__main__":
+    BOT49.run_until_disconnected()
+    
+if __name__ == "__main__":
+    BOT50.run_until_disconnected()
+    
+if __name__ == "__main__":
+    BOT51.run_until_disconnected()
+
+if __name__ == "__main__":
+    BOT52.run_until_disconnected()
+    
+if __name__ == "__main__":
+    BOT53.run_until_disconnected()
+
+if __name__ == "__main__":
+    BOT54.run_until_disconnected()
+    
+if __name__ == "__main__":
+    BOT55.run_until_disconnected()
+    
+if __name__ == "__main__":
+    BOT56.run_until_disconnected()
+
+if __name__ == "__main__":
+    BOT57.run_until_disconnected()
+    
+if __name__ == "__main__":
+    BOT58.run_until_disconnected()
+
+if __name__ == "__main__":
+    BOT59.run_until_disconnected()
+    
+if __name__ == "__main__":
+    BOT60.run_until_disconnected()
+    
